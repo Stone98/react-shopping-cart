aaf03b5ef6a01f932bf26ab4a21df3e4df90e5a1
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-01-21 14:10:46 -0700

    removed log

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Item = props => {
-	console.log(props);
 	return (
 		<div className="shopping-cart_item">
 			<img src={props.image} alt={`${props.title} book`} />
