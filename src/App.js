ff8266b786a73bdf2aa4e6dbdf05278e2cba907a
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-02-18 15:33:00 -0700

    removeItem completed

--- src/App.js
+++ src/App.js
@@ -18,10 +18,15 @@ function App() {
 		setCart([...cart, item])
 	};
 
+	const removeItem = id => {
+		// remove the given item from the cart
+		setCart(cart.filter((item) => item.id !== id));
+	}
+
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<CartContext.Provider value={cart}>
+				<CartContext.Provider value={{ cart, removeItem }}>
 					<Navigation />
 
 					{/* Routes */}
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,7 @@ import CartContext from '../contexts/CartContext';
 import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
-	const cart = useContext(CartContext);
+	const { cart } = useContext(CartContext);
 	console.log(cart);
 	return (
 		<div className="navigation">
